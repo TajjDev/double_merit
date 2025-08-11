21e61060c39daa853a3fbe31ce484c13963a07e9
Tajudeen khabab Akanbi <152615493+TajjDev@users.noreply.github.com>
2025-08-11 11:14:59 +0100

    Update Index.jsx

--- src/pages/index/Index.jsx
+++ src/pages/index/Index.jsx
@@ -5,7 +5,7 @@ import bgOne from "/src/assets/image/bgOne.jpg"
 
 const Index = () => {
     return (
-        <div style={{backgroundImage:`linear-gradient(to left, #00000093, #1fd104bd),url(${bgOne})`}} id='hom'>
+        <div id='hom'>
             <h3>Unlock the Doors to a Future of Excellence</h3>
             <p>Experience a harmonious blend of academic brilliance and moral integrity, shaping well-rounded leaders for tomorrow.</p>
 
@@ -19,3 +19,4 @@ const Index = () => {
 
 
 export default Index
+
