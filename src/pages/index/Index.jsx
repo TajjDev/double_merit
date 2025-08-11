504f613d7815fb51966376f3c8185f072dfad90c
Tajudeen khabab Akanbi <152615493+TajjDev@users.noreply.github.com>
2025-08-11 02:11:19 +0100

    Update Index.jsx

--- src/pages/index/Index.jsx
+++ src/pages/index/Index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Links from '../../component/nav/links/Link';
 import "./indexx.css"
+import bgOne from "/src/assets/image/bgOne.jpg"
 
 const Index = () => {
     return (
-        <div id='hom'>
+        <div style={{backgroundImage:`linear-gradient(to left, #00000093, #1fd104bd),url(${bgOne})`}} id='hom'>
             <h3>Unlock the Doors to a Future of Excellence</h3>
             <p>Experience a harmonious blend of academic brilliance and moral integrity, shaping well-rounded leaders for tomorrow.</p>
 
