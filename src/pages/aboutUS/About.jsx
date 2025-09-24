d93e3c6fe8023291f42469ca6c8c794f67c40984
Tajudeen khabab Akanbi <152615493+TajjDev@users.noreply.github.com>
2025-09-24 21:07:43 +0100

    Add files via upload

--- src/pages/aboutUS/About.jsx
+++ src/pages/aboutUS/About.jsx
@@ -3,7 +3,17 @@ import "./about.css"
 const About = () => {
   return (
     <div id='abt'>
-        <p id='abtP'>About Us</p>
+      <p id='abtP'>About Us</p>
+      <div id="conAbt">
+          <div id="who">
+            <h3>Who Are We?</h3>
+            <p>Double Merit Children Academy, located in 7, Ishaga Road opposite Mabo junction Ojuelegba, Surulere Lagos State, Nigeria, is a private primary school established on September, 2002.The school was founded to provide comprehensive education that integrates academic excellence with moral and intellectual development, grounded in Islamic principles.</p>
+          </div>
+          <div id="found">
+            <h3>Community and Cultural Engagement</h3>
+            <p>Double Merit Children Academy places a strong emphasis on co-curricular activities that enrich students' educational experiences and promote cultural awareness. Events such as the Cultural Dish Day showcase cultural heritage, identity, valus, and expressions, including Visitation to amusement park,amd Visitation to Beach. These activities foster a sense of community and pride among students, encouraging them to explore and appreciate various cultural dimensions.</p>
+          </div>
+        </div>
     </div>
   )
 }
