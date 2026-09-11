c103207a9daf1f9f4ee4983cef794847888680d4
Tajudeen khabab Akanbi <152615493+TajjDev@users.noreply.github.com>
2026-09-11 20:06:08 +0100

    Update Academic.jsx

--- src/pages/academics/Academic.jsx
+++ src/pages/academics/Academic.jsx
@@ -369,7 +369,7 @@ const Academic = () => {
                 <div id="act">
                     
                     <div  id="cultural">
-                        <h4 className='h3'>School Excursion</h4>
+                        <h4 className='h3'>walimah activities</h4>
                         <div id="graduateImg">
                             <img src={image14} alt="Thumbnail" className='thumbnail' onClick={() => openOverlayex(0)} />
                             <img src={image15} alt="Thumbnail" className='thumbnail' onClick={() => openOverlayex(1)} />
