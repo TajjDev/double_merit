b140c922fea28331728962bc28dc98b0d4316e4e
Tajudeen khabab Akanbi <152615493+TajjDev@users.noreply.github.com>
2026-09-11 19:56:57 +0100

    Update About.jsx

--- src/pages/aboutUS/About.jsx
+++ src/pages/aboutUS/About.jsx
@@ -13,27 +13,20 @@ const About = () => {
             <h3>Community and Cultural Engagement</h3>
             <p>Double Merit Children Academy places a strong emphasis on co-curricular activities that enrich students' educational experiences and promote cultural awareness. Events such as the Cultural Dish Day showcase cultural heritage, identity, valus, and expressions, including Visitation to amusement park,and Visitation to Beach. These activities foster a sense of community and pride among students, encouraging them to explore and appreciate various cultural dimensions.</p>
           </div>
-          <div id="ourServ">
-            <h3>🌟Our Services</h3>
+          <div id="ourServ"> <br />
+            <h3>Our Services</h3>
             <ul>
+              <li>📚 Quality Academic Education</li>
               <li>📖 Preparatory Classes</li>
               <li>🧸 Nursery Education</li>
               <li>📚 Primary Education</li>
-              <li>🎓 Junior Secondary School (JSS 1-3)</li>
-              <li>🕌 Islamic Studies</li>
-              <li>📖 Qur’an Recitation and Memorisation</li>
-              <li>🔤 Arabic Language</li>
+              <li>🕌 Madrasah Classes</li>
+              <li>🧸 Daycare Services</li>
+              <li>👨‍👩‍👧 After-School Care Services</li>
               <li>🤲 Islamic Moral and Character Training</li>
-              <li>📝 Academic Coaching & Tutorials</li>
-              <li>☀️ Holiday Lessons / Summer Coaching</li>
               <li>👩‍🏫 Qualified and Caring Teachers</li>
-              <li>🎯 Individual Attention to Pupils</li>
-              <li>💻 Computer/ICT Learning</li>
-              <li>🗣️ Yoruba Language Studies</li>
               <li>🧼 Health Education and Good Hygiene Training</li>
-              <li>🌱 Character Building and Discipline</li>
               <li>👨‍👩‍👧 A Safe, Friendly and Child-Centred Learning Environment</li>
-              <li>DOUBLE MERIT CHILDREN ACADEMY & MADRASAH</li>
             </ul>
           </div>
         </div>
